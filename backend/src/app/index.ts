0448e835f0380368571bb1d53dc73f7ac4871d22
rahiii-dev <rahiofcl@gmail.com>
2025-07-28 18:22:17 +0530

    fix: added client origin for cors

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: process.env.CLIENT_ORIGIN || "http://localhost:5173"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
